30183be6f468d111496b4a39d23997f326ee206e
cedric <fabiocedric@gmail.com>
2024-09-23 23:30:23 -0500

    Completes the MenuItem object

--- src/main/java/com/pekings/pos/object/MenuItem.java
+++ src/main/java/com/pekings/pos/object/MenuItem.java
@@ -1,7 +1,22 @@
 package com.pekings.pos.object;
 
+import java.util.List;
+
 public class MenuItem {
 
     private final long id;
+    private final List<Ingredient> ingredients;
 
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public MenuItem(long id, List<Ingredient> ingredients) {
+        this.id = id;
+        this.ingredients = ingredients;
+    }
 }
