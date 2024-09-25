759187a451bc1a7b924794e68b6b3f4cabed6d16
ceeedric <fabiocedric@gmail.com>
2024-09-25 17:49:53 -0500

    Main now initializes and keeps an instance of Repository

--- src/main/java/com/pekings/pos/Main.java
+++ src/main/java/com/pekings/pos/Main.java
@@ -1,9 +1,21 @@
 package com.pekings.pos;
 
+import com.pekings.pos.storage.PersistentRepository;
+import com.pekings.pos.storage.Repository;
+
 public class Main {
+
+    private static Repository repository;
+
     public static void main(String[] args) throws Exception {
         POSApp posApp = new POSApp();
         posApp.initialize();
 
+        repository = new PersistentRepository();
+        ((PersistentRepository) repository).initialize();
+    }
+
+    public Repository getRepository() {
+        return repository;
     }
 }
