72f5cbd697e9cd5fafebd4d887a3a056d596c2f6
NathanL7 <nathan7@tamu.edu>
2024-09-25 12:00:07 -0500

    additional code/comments into additems.sql

--- src/main/resources/additems.sql
+++ src/main/resources/additems.sql
@@ -4,3 +4,5 @@ INSERT INTO customers (id)
 SELECT nextval(pg_get_serial_sequence('customers', 'id'))
 FROM generate_series(1, 1000);
 
+--- menu table is just:
+-- INSERT INTO menu (name,price) VALUES ('example', 420.69)
