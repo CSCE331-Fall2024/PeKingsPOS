03c171c3455a345561813d27d7868e1c479fe289
jeremiahherm <jeremiahh@tamu.edu>
2024-09-30 16:51:48 -0500

    Login Screen progress, needs button and functionality.

--- src/main/java/com/pekings/pos/POSApp.java
+++ src/main/java/com/pekings/pos/POSApp.java
@@ -2,13 +2,21 @@ package com.pekings.pos;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.util.List;
+
+import static java.awt.Color.*;
 
 public class POSApp extends Application {
 
@@ -19,23 +27,84 @@ public class POSApp extends Application {
     public void start(Stage PrimaryStage) throws Exception{
         window = PrimaryStage;
         window.setTitle("PeKings POS");
+        window.setResizable(false);
+        window.setWidth(1000);
+        window.setHeight(700);
 
-        GridPane grid1 = new GridPane();
-        grid1.setPadding(new Insets(10, 10, 10, 10));
-        grid1.setVgap(10);
-        grid1.setVgap(5);
+        Group root = new Group();
+        Scene login = new Scene(root, 1000, 700);
+        login.setFill(Color.web("#2F2E2E"));
 
-        Label title = new Label("Manager \nLogin");
-        GridPane.setConstraints(title, 0, 1);
+        Text title = new Text("Employee Login");
+        title.setStyle("-fx-font-size: 50px");
+        title.setFill(Color.WHITE);
+        title.setX(290);
+        title.setY(250);
 
-        TextField pin = new TextField("PIN");
-        GridPane.setConstraints(pin, 1, 1);
+        Label usernameLabel = new Label("Username: ");
+        usernameLabel.setLayoutX(310);
+        usernameLabel.setLayoutY(300);
+        usernameLabel.setStyle("-fx-font-size: 30px");
+        usernameLabel.setTextFill(Color.WHITE);
 
-        grid1.getChildren().addAll(title, pin);
+        TextField usernameBox = new TextField("");
+        usernameBox.setLayoutX(460);
+        usernameBox.setLayoutY(310);
 
-        login = new Scene(grid1, 1000, 700);
+        Label passwordLabel = new Label("Password: ");
+        passwordLabel.setLayoutX(310);
+        passwordLabel.setLayoutY(350);
+        passwordLabel.setStyle("-fx-font-size: 30px");
+        passwordLabel.setTextFill(Color.WHITE);
+
+        TextField passwordBox = new TextField("");
+        passwordBox.setLayoutX(460);
+        passwordBox.setLayoutY(360);
+
+
+
+//        StackPane rootPane = new StackPane();
+//        login = new Scene(rootPane, 1000, 700);
+//        Pane titlePane = new Pane();
+//        Pane loginPane = new Pane();
+//        rootPane.getChildren().addAll(titlePane, loginPane);
+//
+//        Label title = new Label("Login");
+//        title.setAlignment(Pos.CENTER);
+//        titlePane.getChildren().add(title);
+//        titlePane.set
+
+
+
+//        GridPane grid1 = new GridPane();
+//        grid1.setPadding(new Insets(10, 10, 10, 10));
+//        grid1.setVgap(10);
+//        grid1.setHgap(2);
+
+//        Label title = new Label("Login");
+//        title.setStyle("-fx-font-size: 50px");
+//        GridPane.setConstraints(title, 0, 0);
+//
+//        Label usernameLabel = new Label("Username: ");
+//        GridPane.setConstraints(usernameLabel, 0, 5);
+//
+//        TextField usernameLogin = new TextField("");
+//        GridPane.setConstraints(usernameLogin, 2, 5);
+//
+//        Label passwordLabel = new Label("Password: ");
+//        GridPane.setConstraints(passwordLabel, 0, 6);
+//
+//        TextField passwordLogin = new TextField("");
+//        GridPane.setConstraints(passwordLogin, 2, 6);
+//
+//        grid1.getChildren().addAll(title, usernameLabel, usernameLogin, passwordLabel, passwordLogin);
+//        grid1.setAlignment(Pos.CENTER);
+
+//        login = new Scene(grid1, 1000, 700);
+//        window.setScene(login);
+
+        root.getChildren().addAll(title, usernameLabel, passwordLabel, usernameBox, passwordBox);
         window.setScene(login);
-
         window.show();
     }
 
