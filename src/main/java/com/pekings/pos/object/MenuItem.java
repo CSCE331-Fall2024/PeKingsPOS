2dd8b064b1c552e1c70583dfe79520f477d1cd0b
ceeedric <fabiocedric@gmail.com>
2024-10-02 17:36:16 -0500

    Removes addIngredient and removeIngredient from MenuItem. Updates should be done through the Repository interface

--- src/main/java/com/pekings/pos/object/MenuItem.java
+++ src/main/java/com/pekings/pos/object/MenuItem.java
@@ -21,14 +21,6 @@ public class MenuItem {
         return new ArrayList<>(ingredients);
     }
 
-    public void addIngredient(Ingredient ingredient) {
-        ingredients.add(ingredient);
-    }
-
-    public void removeIngredient(Ingredient ingredient) {
-        ingredients.remove(ingredient);
-    }
-
     public long getId() {
         return id;
     }
