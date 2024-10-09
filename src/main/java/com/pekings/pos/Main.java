c2ab19e49d3a5bf7d92d4d4a4e6e902864108d61
ceeedric <fabiocedric@gmail.com>
2024-10-09 16:19:10 -0500

    Revert "Merge pull request #9 from CSCE331-Fall2024/front-end/connect-screens"
    This reverts commit 28bc3a79c1fc278f5a14a1022f0a2fbb7594032c, reversing
    changes made to 816b8e70fda8a7a9507e9288ead09e4057bd404a.

--- src/main/java/com/pekings/pos/Main.java
+++ src/main/java/com/pekings/pos/Main.java
@@ -2,6 +2,7 @@ package com.pekings.pos;
 
 import com.pekings.pos.storage.PersistentRepository;
 import com.pekings.pos.storage.Repository;
+
 import java.sql.SQLException;
 
 public class Main {
--- src/main/java/com/pekings/pos/POSApp.java
+++ src/main/java/com/pekings/pos/POSApp.java
@@ -1,16 +1,26 @@
 package com.pekings.pos;
 
+import com.pekings.pos.storage.Repository;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class POSApp extends Application {
 
-    @Override
-    public void start(Stage PrimaryStage) throws Exception {
-        new Login(PrimaryStage);
-    }
+    private Repository repo;
 
     public void initialize(String[] args) {
         launch(args);
     }
+
+    public void start(Stage stage) throws Exception {
+        new Login(stage);
+        repo = Main.getRepository(); // Get repo from Main class
+        stage.setTitle("PeKing POS System");
+
+        Manager manager = new Manager(repo);
+        stage.setScene(manager.createManagerScene(stage));
+        stage.setResizable(false);
+        stage.show();
+    }
 }
+
